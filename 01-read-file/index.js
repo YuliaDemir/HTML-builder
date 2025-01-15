87abaf7f37baa2337127081ee75c5cf1a4e76183
Yulia  Demir <Nightelfin91@gmail.com>
2025-01-15 17:05:17 +0300

    feat: read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('01-read-file/text.txt', 'utf8');
+
+readStream.on('data', (chunk) => {
+    console.log(chunk);
+});
+
+readStream.on('error', (err) => {
+    console.error('Ошибка при чтении файла:', err);
+});
